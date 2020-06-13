645b3e0e5d26bcd1c3045b1d45ba48c2b07a8f2e
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-13 11:28:35 -0500

    Added NumericInput to repository

--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -2,3 +2,27 @@
 import React from 'react';
 //import connect from redux in order to make global variables.
 import {connect} from 'react-redux';
+import {Component} from 'react';
+
+class Feelings extends Component{
+
+    render(){
+        return (
+            <div>
+                <h1>How are you feeling today?</h1>
+                <div id='feelingInput'>
+                    <p>Feeling:</p>
+                <NumericInput min={0} max={5} value={1}/>
+                <button >Next</button>
+                </div>
+
+
+            </div>
+        )
+    }
+
+}
+
+
+export default connect()(Feelings);
+
