54f4b7935f87aea249956b5fde2ec9b209a46d27
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-13 11:15:47 -0500

    Inital File Structure created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+//import the hash router from react-router-dom in order to switch inbetween commponents by url path
+import { HashRouter as Router, Route, Link} from 'react-router-dom'; 
 import './App.css';
+//import components from JS pages in order to link the routes.
+import Feelings from '../Feelings/Feelings';
 
 class App extends Component {
   render() {
@@ -11,6 +15,14 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+        <section>
+          {/* establish routes for all the feedback pages */}
+          <Router>
+            <Route path='/feelings' component={Feelings}/>
+            {/* <Route path='/comments' component={Comments}/> */}
+          </Router>
+
+        </section>
       </div>
     );
   }
---
+++ src/components/Feelings/Feelings.js
@@ -0,0 +1,4 @@
+//import dependencies
+import React from 'react';
+//import connect from redux in order to make global variables.
+import {connect} from 'react-redux';
