75dd82a5b05884da9d8e9d19d80c6d96cba16281
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-13 11:30:30 -0500

    installed react-redux

--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -3,6 +3,8 @@ import React from 'react';
 //import connect from redux in order to make global variables.
 import {connect} from 'react-redux';
 import {Component} from 'react';
+//import NumericInput from react-numeric-input in order to use it in your input form
+import NumericInput from 'react-numeric-input';
 
 class Feelings extends Component{
 
@@ -10,11 +12,11 @@ class Feelings extends Component{
         return (
             <div>
                 <h1>How are you feeling today?</h1>
-                <div id='feelingInput'>
+                <form id='feelingInputForm'>
                     <p>Feeling:</p>
                 <NumericInput min={0} max={5} value={1}/>
                 <button >Next</button>
-                </div>
+                </form>
 
 
             </div>
