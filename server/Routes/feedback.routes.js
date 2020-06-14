56cc2cc586440feaf34a3ee51e888e6bf8eb0352
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-14 17:07:27 -0500

    Post to Database working

--- server/Routes/feedback.routes.js
+++ server/Routes/feedback.routes.js
@@ -5,33 +5,23 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 //Post all feeback entries to the database with a Post request
-router.post('/', async (req, res) =>{
-    const client = await pool.connect();
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const feelings = req.body.feelings;
+    const understanding = req.body.understanding;
+    const support = req.body.support;
+    const comments = req.body.comments
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES($1, $2, $3, $4)`
 
-    try {
-        const{
-            feeling,
-            understanding,
-            support,
-            comments,
-            flagged,
-            date
-        } = req.body;
-        await client.query('BEGIN')
-        const feedbackInsertResults = await client.query(`INSERT INTO "feedback" ("feeling","understanding","support","comments","flagged","date")
-        VALUES ($1, $2, $3, $4, $5, $6)
-        RETURNING id:`, [feeling, understanding, support, comments, flagged, date]);
-        const feedbackId = feedbackInsertResults.rows[0].id;
+    pool.query(queryText, [feelings, understanding, support, comments])
+        .then((results) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database querry ${queryText}`, error);
+            res.sendStatus(500);
+        });
+});
 
-        await client.query('COMMIT')
-        res.sendStatus(201);
-    }catch (error){
-        await client.query('ROLLBACK')
-        console.log('Error POST /api/order', error);
-        res.sendStatus(500);
-    } finally {
-        client.release()
-    }
-})
 
 module.exports = router;
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Component} from 'react';
 import {connect} from 'react-redux';
+import Axios from 'axios';
 
 class Review extends Component{
     componentDidMount =() =>{
@@ -10,10 +11,29 @@ class Review extends Component{
     //with submitFeedback function send the feedback to the database and navigate to the next page
     submitFeedback = () =>{
         //grab submission time on button click with Date()
-        var submissionDate = Date();
+        //var submissionDate = Date();
+        //create a feedback object of all the information from the redux store reducers in order to pass
+        //the data server side with a post request
+        const feedbackObject = {
+            feelings: this.props.reduxStore.feelingsReducer,
+            understanding: this.props.reduxStore.understandingReducer,
+            support: this.props.reduxStore.supportReducer,
+            comments: this.props.reduxStore.commentsReducer,
+            //flagged: false,
+            //submissionDate: submissionDate
+        }
 
         //write a post request that post info to the database
-        
+        Axios({
+            method: 'POST',
+            url: '/api/feedback',
+            data: feedbackObject
+        }).then((response) => {
+            console.log('response', response)
+        }).catch((error) => {
+            console.log("error with axios Post", error);
+        })
+
         //
         this.props.history.push('/submissionpage');
     }
